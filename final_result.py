091b1cd85fa95a1b1e1cb65d0000c1219e06533f
Amir01 <iran.volleyball79@gmail.com>
2022-06-27 20:33:25 +0430

    integrate modules

--- final_result.py
+++ final_result.py
@@ -1,3 +1,7 @@
+from fuzzification import fuzzify
+from inference import import_rules, rule_evaluation
+from defuzzification import defuzzify_health
+
 class ProvideResult(object):
     def __new__(cls):
         if not hasattr(cls, 'instance'):
@@ -6,4 +10,8 @@ class ProvideResult(object):
 
     @staticmethod
     def get_final_result(input_dict: dict) -> str:
-        pass
+        input_fuzzified = fuzzify(input_dict)
+        rules_dict = import_rules('./rules.fcl')
+        inference_dict = rule_evaluation(rules_dict, input_fuzzified)
+        health_output = defuzzify_health(inference_dict)
+        return health_output
