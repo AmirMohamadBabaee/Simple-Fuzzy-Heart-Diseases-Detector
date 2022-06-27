c7935310f0d4633f66b8a50e4e4b07321ad4f5d0
Amir01 <iran.volleyball79@gmail.com>
2022-06-27 19:00:49 +0430

    complete rule_evaluation function in inference.py

--- inference.py
+++ inference.py
@@ -1,7 +1,7 @@
 import re
 
 def rule_extractor(rule):
-    match_parts = re.match(r'RULE\s(?P<rule_num>\d+):\sIF\s*\((?P<rule_hypothesis>.+)\)\sTHEN\s(?P<rule_conclusion>.+);', rule)
+    match_parts = re.match(r'RULE\s*(?P<rule_num>\d+):\s*IF\s*\((?P<rule_hypothesis>.+)\)\sTHEN\s(?P<rule_conclusion>.+);', rule)
     match_dict = match_parts.groupdict()
     rule_num = match_dict['rule_num']
     hypothesis = match_dict['rule_hypothesis']
@@ -48,6 +48,60 @@ def import_rules(rules_path):
     # print(rules_dict)
     return rules_dict
 
+def rule_evaluation(rules_dict :dict, input_fuzzified_dict : dict):
+    conclusion_dict = {
+        'health': {
+            'healthy': 0,
+            'sick_1' : 0,
+            'sick_2' : 0,
+            'sick_3' : 0,
+            'sick_4' : 0,
+        }
+    }
+    for rule_num, rule in rules_dict.items():
+        rule_power = 0
+
+        if rule['rule_hypothesis'].get('operator'):
+            hypothesis  = rule['rule_hypothesis']
+            conclusion  = rule['rule_conclusion']
+
+            hypothesis_first_variable       = list(hypothesis.keys())[0]
+            hypothesis_first_value          = hypothesis[hypothesis_first_variable]
+            hypothesis_second_variable      = list(hypothesis.keys())[1]
+            hypothesis_second_value         = hypothesis[hypothesis_second_variable]
+            hypothesis_operator             = hypothesis['operator']
+
+            first_variable_rule_power       = input_fuzzified_dict[hypothesis_first_variable][hypothesis_first_value]
+            second_variable_rule_power      = input_fuzzified_dict[hypothesis_second_variable][hypothesis_second_value]
+
+            if hypothesis_operator.upper() == 'AND':    # compute min of values
+                rule_power = min(first_variable_rule_power, second_variable_rule_power)
+            elif hypothesis_operator.upper() == 'OR':   # compute max of values
+                rule_power = max(first_variable_rule_power, second_variable_rule_power)
+            else:
+                return
+
+            conclusion_variable     = list(conclusion.keys())[0]
+            conclusion_value        = list(conclusion.values())[0]
+            conclusion_current_value = conclusion_dict[conclusion_variable][conclusion_value]
+            conclusion_dict[conclusion_variable][conclusion_value] = max(rule_power, conclusion_current_value)
+            
+        else:
+            hypothesis  = rule['rule_hypothesis']
+            conclusion  = rule['rule_conclusion']
+
+            hypothesis_variable     = list(hypothesis.keys())[0]
+            hypothesis_value        = list(hypothesis.values())[0]
+            rule_power              = input_fuzzified_dict[hypothesis_variable][hypothesis_value]
+
+            conclusion_variable     = list(conclusion.keys())[0]
+            conclusion_value        = list(conclusion.values())[0]
+            conclusion_current_value = conclusion_dict[conclusion_variable][conclusion_value]
+            conclusion_dict[conclusion_variable][conclusion_value] = max(rule_power, conclusion_current_value)
+
+    return conclusion_dict
+
 
 if __name__ == "__main__":
-    import_rules('./rules.fcl')
+    rules_dict = import_rules('./rules.fcl')
+    print(rules_dict)
